ac669a6a58cb298c9c20e2c82778adb18a73a957
guptaditya27 <adgupta285@gmail.com>
2026-04-17 03:08:19 +0530

    fix: update Google OAuth Client ID to new credentials

--- my-dsa-roadmap/src/main.jsx
+++ my-dsa-roadmap/src/main.jsx
@@ -4,7 +4,7 @@ import { GoogleOAuthProvider } from '@react-oauth/google'
 import './index.css'
 import App from './App.jsx'
 
-const clientId = import.meta.env.VITE_GOOGLE_CLIENT_ID || "385499713123-4shivo1mpl77eng1jqidfvhojiqk00lh.apps.googleusercontent.com";
+const clientId = import.meta.env.VITE_GOOGLE_CLIENT_ID || "385499713123-7kcfgl8dvqv0rfd8lpghmgg6m9l1geqa.apps.googleusercontent.com";
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
